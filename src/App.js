113dbdbb678de5670cfe999725346dbbc8e02d81
Sonish <sonishn213@gmail.com>
2022-06-09 18:17:23 +0530

    initial commit

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import React from "react";
+function App() {
+  return (
+    <div className="">
+      <h1>hello</h1>
+    </div>
+  );
+}
+
+export default App;
---
+++ tailwind.config.js
@@ -0,0 +1,20 @@
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    screens: {
+      sm: "480px",
+      md: "768px",
+      lg: "976px",
+      xl: "1440px",
+    },
+    extend: {
+      colors: {
+        "s-dark-blue": "#14213D",
+        "s-orange": "#FCA311",
+        "s-dark-grey": "#E5E5E5",
+        "s-light-grey": "#F2F2F2",
+      },
+    },
+  },
+  plugins: [],
+};
