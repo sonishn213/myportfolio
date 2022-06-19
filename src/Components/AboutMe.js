0722bc146a8f805dd5a7c1966f326faf2be77d87
Sonish <sonishn213@gmail.com>
2022-06-19 21:34:08 +0530

    Add text about me

--- src/Components/AboutMe.js
+++ src/Components/AboutMe.js
@@ -7,13 +7,8 @@ const AboutMe = () => {
         <div className="max-w-4xl mx-auto">
           <Title value="About me" className="mb-16" />
           <p className="text-lg text-slate-700 text-center">
-            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Unde
-            officia ab expedita odit, iste neque aut? Esse inventore, enim
-            tempora, pariatur laborum doloribus ullam et amet ducimus voluptatum
-            exercitationem facilis. Lorem ipsum dolor sit, amet consectetur
-            adipisicing elit. Unde officia ab expedita odit, iste neque aut?
-            Esse inventore, enim tempora, pariatur laborum doloribus ullam et
-            amet ducimus voluptatum exercitationem facilis.
+            Hi, Im shwanees. Im a bca graduate, graduated in 2021-22.
+            <br /> Im currently studying frontend webdevelopment using Reactjs.
           </p>
         </div>
       </div>
